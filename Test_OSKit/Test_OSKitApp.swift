4fb2206fef7ca580e26a6420b44ad8212ad0f5a3
Gokul Nair <gokul.getpowerplay.in>
2023-06-16 15:55:20 +0530

    Initial Commit

---
+++ Test_OSKit/Test_OSKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_OSKitApp.swift
+//  Test_OSKit
+//
+//  Created by Gokul Nair on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_OSKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
